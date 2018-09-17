459764f401bd5346a60871dba27cf5fc869b2e61
stampstang5415 <stampstang5415@gmail.com>
2018-09-17 09:30:31 +0700

    ss5

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 var express = require('express');
 var app = express();
-var pgp = require('pg-promise')();
-var db = pgp('postgres://nkwnjxuiidwrns:b72b4de42f726173c9acee8a85dd10ed1c8dc1a2ab7402a6feebbbccb8b14f85@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d34ii1v5fr4h1e?ssl=true');
-//app.use(express.static('www'));
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host     : 'www.db4free.net',
+  user     : 's140390',
+  password : 'abc123**',
+  database : 'db140390'
+});
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.render('pages/index');
+    res.render('pages/Home');
 });
 
-app.get('/about', function(req, res) {
+app.get('/students', function(req, res) {
     var name = 'Patiphan Wibunchat';
     var hobbies = ['Music','Movie','Programming'];
     var bdate = '09/10/2018';
     res.render('pages/about', {name :name , hobbies : hobbies , bdate : bdate});
 });
-// Display all products
+
 app.get('/products', function(req, res) {
     var id = req.param('id');
     var sql = 'select * from products';
